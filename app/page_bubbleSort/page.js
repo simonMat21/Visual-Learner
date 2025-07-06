099d256887ab4839645fe5f9e751ce56af44d6ce
simonM21 <simonmattekkatt21@gmail.com>
2025-07-06 21:14:19 +0530

    changed bubble insertand selection sort to use tideon

--- app/page_bubbleSort/page.js
+++ app/page_bubbleSort/page.js
@@ -2,16 +2,46 @@
 
 import { useState } from "react";
 
-import P5Sketch_bubbleSort from "./P5Sketch_bubbleSort";
+import { Slider } from "@/components/ui/slider";
 import NumberInput from "../components/NumberInput";
 
+import P5Sketch from "./P5Sketch";
+
 export default function Home() {
-  const [data, setData] = useState([]);
+  const [AEBool, setAEBool] = useState(true);
+  const [addForm, setAddForm] = useState({ val: [], pos: 0, start: false });
+  const [animSpd, setAnimSpd] = useState(1);
+
+  const updateForm = (n, key, value) => {
+    if (key !== "start" || AEBool) {
+      if (n == 1) {
+        setAddForm((prev) => ({ ...prev, [key]: value }));
+      }
+    }
+  };
   return (
     <main>
       <h1 className="text-3xl font-bold underline">Algo visuvalizor</h1>
-      <NumberInput onSubmit={(arr) => setData(arr)} />
-      <P5Sketch_bubbleSort inputArray={data} />
+      <NumberInput
+        onSubmit={(arr) => {
+          updateForm(1, "val", arr);
+          updateForm(1, "start", true);
+          setTimeout(() => updateForm(1, "start", false), 10);
+        }}
+      />
+      <Slider
+        defaultValue={[1]}
+        min={0.5}
+        max={1.5}
+        step={0.01}
+        onValueChange={([val]) => setAnimSpd(val)}
+        className="w-64 h-6 "
+      />
+      <P5Sketch
+        add={addForm}
+        animSpd={animSpd}
+        actionExicutable={(b) => setAEBool(b)}
+      />
     </main>
   );
 }
