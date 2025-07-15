b0862d57851da826a94c15b5d063f042d2bc7763
simonM21 <simonmattekkatt21@gmail.com>
2025-07-15 22:14:22 +0530

    fixed bucket sort

--- app/page_BucketSort/P5Sketch.js
+++ app/page_BucketSort/P5Sketch.js
@@ -1,18 +1,10 @@
 "use client";
 
 import React, { useRef, useEffect } from "react";
-import { Animator } from "../components/Animator";
+import { Animator, a2o } from "../components/Tideon";
 
-export default function P5Sketch({
-  dlt,
-  add,
-  srch,
-  animSpd,
-  actionExicutable,
-}) {
+export default function P5Sketch({ add, animSpd, actionExicutable }) {
   const sketchRef = useRef(null);
-  const deleteRef = useRef(dlt);
-  const searchRef = useRef(srch);
   const addRef = useRef(add);
   const animSpdRef = useRef(animSpd);
 
@@ -33,12 +25,18 @@ export default function P5Sketch({
 
           show() {
             P.push();
-            P.noFill();
+            P.fill(this.col[0], this.col[1], this.col[2], this.opacity - 150);
             P.strokeWeight(3);
             P.stroke(this.col[0], this.col[1], this.col[2], this.opacity);
             P.rectMode(P.CENTER);
-            P.rect(this.x, this.y, 40);
+            P.rect(this.x, this.y, 40, 30);
             P.pop();
+          }
+
+          setUp(x, y, op) {
+            this.x = x;
+            this.y = y;
+            this.opacity = op;
           }
         }
 
@@ -55,7 +53,7 @@ export default function P5Sketch({
             P.stroke(0, 0, 0, this.opacity);
             P.fill(200, this.opacity);
             P.rectMode(P.CENTER);
-            P.rect(this.x, this.y, 30);
+            P.rect(this.x, this.y, 40, 30);
             //---------text---------
             P.fill(255, 105, 0, this.opacity);
             P.strokeWeight(1);
@@ -67,6 +65,8 @@ export default function P5Sketch({
           }
         }
         class HT_box {
+          static maxVal = 100;
+          static minVal = 0;
           constructor(val, x, y, op) {
             this.val = val;
             this.x = x;
@@ -77,11 +77,14 @@ export default function P5Sketch({
             P.push();
             //---------rect---------
             P.stroke(100, this.opacity);
-            P.fill(200, this.opacity);
+            P.fill(
+              P.map(this.val, HT_box.minVal, HT_box.maxVal, 225, 50),
+              this.opacity
+            );
             P.rectMode(P.CENTER);
             P.rect(this.x, this.y, 30);
             //---------text---------
-            P.fill(100, this.opacity);
+            P.fill(255, 105, 0, this.opacity);
             P.strokeWeight(1);
             P.textAlign(P.CENTER, P.CENTER);
             P.textSize(15);
@@ -97,51 +100,156 @@ export default function P5Sketch({
             this.table = Array(this.size)
               .fill(null)
               .map(() => []);
-            this.x = 40;
-            this.y = 50;
-            this.vgap = 70;
+            this.x = 300;
+            this.y = 30;
+            this.vgap = 35;
           }
 
           setUp() {
             for (let i = 0; i < this.size; i++) {
               // Each bucket is an array of HT_box objects (to visualize chaining)
-              this.table[i] = [new HT_Index_box(i, 300, 50 + 45 * i, 255)];
+              this.table[i] = [
+                new HT_Index_box(
+                  `<${(i + 1) * 10}`,
+                  this.x,
+                  this.y + this.vgap * i,
+                  255
+                ),
+              ];
             }
           }
 
           hash(val) {
-            return val % this.size;
+            if (val < 10) return 0;
+            if (val < 20) return 1;
+            if (val < 30) return 2;
+            if (val < 40) return 3;
+            if (val < 50) return 4;
+            if (val < 60) return 5;
+            if (val < 70) return 6;
+            if (val < 80) return 7;
+            if (val < 90) return 8;
+            if (val < 100) return 9;
           }
 
           insert(val) {
+            let nbox = new HT_box(val, 100, 200, 0);
+            animator.addStage({
+              funcName: "insert",
+              Args: [nbox],
+            });
             let idx = this.hash(val);
             // Check if val already exists (optional)
             for (let box of this.table[idx]) {
               if (box.val === val) return; // avoid duplicates
             }
             let chainLen = this.table[idx].length;
-            this.table[idx].push(
-              new HT_box(val, 300 + chainLen * 50, 50 + 45 * idx, 255)
-            );
+            animator.addStage({
+              funcName: "add",
+              Args: [
+                nbox,
+                this.x + 20 + chainLen * 30,
+                this.y + this.vgap * idx,
+              ],
+            });
+            this.table[idx].push(nbox);
           }
 
-          search(val) {
-            let idx = this.hash(val);
-            for (let box of this.table[idx]) {
-              if (box.val === val) return true;
-            }
-            return false;
+          // search(val) {
+          //   ckr.setUp(100, 200, 0);
+          //   animator.addStage({
+          //     funcName: "insert",
+          //     Args: [ckr],
+          //   });
+          //   let idx = this.hash(val);
+          //   animator.addStage({
+          //     funcName: "check",
+          //     Args: [this.table[idx][0], ckr],
+          //   });
+          //   for (let box of this.table[idx]) {
+          //     animator.addStage({
+          //       funcName: "check",
+          //       Args: [box, ckr],
+          //     });
+          //     if (box.val === val) {
+          //       animator.addStage({
+          //         funcName: "found",
+          //         Args: [ckr],
+          //       });
+          //       return box;
+          //     }
+          //   }
+          //   animator.addStage({
+          //     funcName: "notfound",
+          //     Args: [ckr],
+          //   });
+          //   return null;
+          // }
+
+          // delete(val) {
+          //   let idx = this.hash(val);
+          //   let dBox = this.search(val);
+          //   animator.addStage({
+          //     funcName: "remove",
+          //     Args: [dBox],
+          //   });
+          //   animator.addStage({
+          //     funcName: "reset",
+          //     Args: [
+          //       this.table[idx].filter(
+          //         (_, id) =>
+          //           id > this.table[idx].findIndex((item) => item.val === val)
+          //       ),
+          //     ],
+          //   });
+          //   animator.standAloneFunc(1, () => {
+          //     this.table[idx] = this.table[idx].filter(
+          //       (box) => box.val !== val
+          //     );
+          //   });
+          // }
+
+          sortHT() {
+            ckr.setUp(100, 200, 0);
+            animator.addStage({
+              funcName: "insert",
+              Args: [ckr],
+            });
+            this.table.forEach((arr) => {
+              animator.addStage({
+                funcName: "check",
+                Args: [arr[0], ckr],
+              });
+              animator.addStage({
+                funcName: "Sort",
+                Args: [arr.filter((_, id) => id > 0)],
+              });
+            });
+            animator.addStage({
+              funcName: "found",
+              Args: [ckr],
+            });
           }
 
-          delete(val) {
-            let idx = this.hash(val);
-            this.table[idx] = this.table[idx].filter((box) => box.val !== val);
+          bringHTdown() {
+            let xSet = 0;
+            this.table.forEach((arr) => {
+              animator.addStage({
+                funcName: "bringDown",
+                Args: [
+                  arr.filter((_, id) => id > 0).sort((a, b) => a.val - b.val),
+                  300 + 40 * xSet,
+                ],
+              });
+              xSet += arr.length - 1;
+            });
           }
 
           reset() {
             for (let i = 0; i < this.size; i++) {
               this.table[i] = [];
             }
+            this.setUp();
           }
 
           show() {
@@ -155,34 +263,114 @@ export default function P5Sketch({
 
         //-----------------------------------------------------------------------------------------------
 
-        //-----------------------------------------------------------------------------------------------
-
-        function check(_, [a, ckr]) {
+        function check([a, ckr]) {
           return animator.animationSequence([
             animator.delay(10),
-            animator.to(40, [[ckr, a.x, a.y, 255]]),
+            animator.to(30, [a2o(ckr, a.x, a.y, 255)]),
           ]);
         }
 
-        function insert(_, [a]) {
+        function insert([a]) {
           return animator.animationSequence([
-            animator.animate(1, [[a, 0, -50, 0]]),
-            animator.animate(20, [[a, 0, 50, 255]]),
+            animator.animate(1, [a2o(a, 0, -50, 0)]),
+            animator.animate(20, [a2o(a, 0, 50, 255)]),
+          ]);
+        }
+
+        function add([a, x, y]) {
+          return animator.animationSequence([
+            animator.delay(20),
+            animator.to(20, [a2o(a, a.x, y, 255)]),
+            animator.delay(20),
+            animator.to(50, [a2o(a, x, a.y, 255)]),
+          ]);
+        }
+
+        function reset([arr]) {
+          return animator.animationSequence([
+            animator.animate(
+              20,
+              arr.map((box) => a2o(box, -30, 0, 0))
+            ),
+          ]);
+        }
+
+        function remove([a]) {
+          return animator.animationSequence([
+            animator.animate(20, [a2o(a, 0, -50, -255)]),
+          ]);
+        }
+
+        function found([ckr]) {
+          return animator.animationSequence([
+            animator.animateFunc(1, () => {
+              ckr.col = [0, 255, 0];
+            }),
+            animator.animate(40, [a2o(ckr, 0, 0, -255)]),
+            animator.animateFunc(1, () => {
+              ckr.col = [0, 0, 255];
+            }),
+          ]);
+        }
+
+        function notfound([ckr]) {
+          return animator.animationSequence([
+            animator.animateFunc(1, () => {
+              ckr.col = [255, 0, 0];
+            }),
+            animator.animate(40, [a2o(ckr, 0, 0, -255)]),
+            animator.animateFunc(1, () => {
+              ckr.col = [0, 0, 255];
+            }),
+          ]);
+        }
+
+        function Sort([arr]) {
+          const pos = arr
+            .map((item, i) => animator.initialVal(item.x, i))
+            .sort((a, b) => a - b);
+          return animator.animationSequence([
+            animator.to(
+              40,
+              arr
+                .sort((a, b) => a.val - b.val)
+                .map((item, index) => {
+                  return a2o(item, pos[index], item.y, item.opacity);
+                })
+            ),
+          ]);
+        }
+
+        function bringDown([arr, x = 300]) {
+          return animator.animationSequence([
+            animator.to(
+              30,
+              arr.map((a, i) => {
+                return a2o(a, x + 40 * i, 400, 255);
+              })
+            ),
           ]);
         }
 
         //------------------------------------------------------------------------------------------------
 
         let ht = new HashTable();
-        let listOfActions = [];
-
+        let ckr = new checker();
         let animator;
+
         P.setup = () => {
           P.createCanvas(1000, 500);
           animator = new Animator();
           animator.functionsDictionary = {
             insert: insert,
             check: check,
+            add: add,
+            reset: reset,
+            remove: remove,
+            found: found,
+            notfound: notfound,
+            Sort: Sort,
+            bringDown: bringDown,
           };
           ht.setUp();
         };
@@ -191,7 +379,7 @@ export default function P5Sketch({
           P.frameRate(60);
           P.background(220, 34, 72);
 
-          animator.mainAnimationSequence(listOfActions);
+          animator.mainAnimationSequence();
           animator.setDelayMult(animSpdRef.current);
 
           if (animator.executing) {
@@ -201,22 +389,22 @@ export default function P5Sketch({
           }
 
           if (addRef.current.start) {
-            ht.insert(addRef.current.val);
+            ht.reset();
+            animator.standAloneFunc(1, () => {
+              addRef.current.val.forEach((element) => {
+                ht.insert(element);
+              });
+            });
+            animator.standAloneFunc(1, () => {
+              ht.sortHT();
+            });
+            animator.standAloneFunc(1, () => {
+              ht.bringHTdown();
+            });
             addRef.current.start = false;
           }
-
-          if (deleteRef.current.start) {
-            ht.delete(deleteRef.current.val);
-            deleteRef.current.start = false;
-          }
-
-          if (searchRef.current.start) {
-            console.log(ht.search(searchRef.current.val));
-            ht.search(deleteRef.current.val);
-
-            searchRef.current.start = false;
-          }
           ht.show();
+          ckr.show();
         };
       };
 
@@ -230,10 +418,8 @@ export default function P5Sketch({
 
   useEffect(() => {
     addRef.current = add;
-    deleteRef.current = dlt;
-    searchRef.current = srch;
     animSpdRef.current = animSpd;
-  }, [dlt, add, srch, animSpd]);
+  }, [add, animSpd]);
 
   return <div ref={sketchRef} className="canvas-wrapper"></div>;
 }
--- app/page_BucketSort/page.js
+++ app/page_BucketSort/page.js
@@ -2,96 +2,33 @@
 
 import { useState } from "react";
 
-import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
 import { Slider } from "@/components/ui/slider";
+import NumberInput from "../components/NumberInput";
 
 import P5Sketch from "./P5Sketch";
 
 export default function Home() {
   const [AEBool, setAEBool] = useState(true);
-  const [addForm, setAddForm] = useState({ val: 0, pos: 0, start: false });
-  const [searchForm, setSearchForm] = useState({
-    val: 0,
-    start: false,
-  });
-  const [deleteForm, setDeleteForm] = useState({
-    val: 0,
-    start: false,
-  });
-
+  const [addForm, setAddForm] = useState({ val: [], pos: 0, start: false });
   const [animSpd, setAnimSpd] = useState(1);
 
   const updateForm = (n, key, value) => {
     if (key !== "start" || AEBool) {
       if (n == 1) {
         setAddForm((prev) => ({ ...prev, [key]: value }));
-      } else if (n == 2) {
-        setDeleteForm((prev) => ({ ...prev, [key]: value }));
-      } else if (n == 3) {
-        setSearchForm((prev) => ({ ...prev, [key]: value }));
       }
     }
   };
   return (
     <main className="main">
       <h1 className="heading tshad">Algo Visualiser</h1>
-      {/* <NumberInput onSubmit={(arr) => setData(arr)} /> */}
-      <div className="flex gap-10">
-        <div className="flex items-center gap-2">
-          <Input
-            className="inpbox"
-            placeholder="Enter number to search"
-            onChange={(e) => updateForm(1, "val", Number(e.target.value))}
-          />
-          <Button
-            className="dobtn"
-            onClick={() => {
-              updateForm(1, "start", true);
-              setTimeout(() => updateForm(1, "start", false), 10);
-            }}
-          >
-            add
-          </Button>
-        </div>
-
-        <div className="flex items-center gap-2">
-          <Input
-            className="inpbox"
-            placeholder="Enter position"
-            // value={form.pos}
-            onChange={(e) => updateForm(3, "val", Number(e.target.value))}
-          />
-          <Button
-            onClick={() => {
-              updateForm(3, "start", true);
-              setTimeout(() => updateForm(3, "start", false), 10);
-            }}
-            className="dobtn"
-          >
-            search
-          </Button>
-        </div>
-
-        <div className="flex items-center gap-2">
-          <Input
-            className="inpbox"
-            placeholder="Enter position"
-            // value={form.pos}
-            onChange={(e) => updateForm(2, "val", Number(e.target.value))}
-          />
-          <Button
-            onClick={() => {
-              updateForm(2, "start", true);
-              setTimeout(() => updateForm(2, "start", false), 10);
-            }}
-            className="dobtn"
-          >
-            delete
-          </Button>
-        </div>
-      </div>
-
+      <NumberInput
+        onSubmit={(arr) => {
+          updateForm(1, "val", arr);
+          updateForm(1, "start", true);
+          setTimeout(() => updateForm(1, "start", false), 10);
+        }}
+      />
       <Slider
         defaultValue={[1]}
         min={0.5}
@@ -102,8 +39,6 @@ export default function Home() {
       />
       <P5Sketch
         add={addForm}
-        dlt={deleteForm}
-        srch={searchForm}
         animSpd={animSpd}
         actionExicutable={(b) => setAEBool(b)}
       />
